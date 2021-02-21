b51b0c4ef811cd5443cb7aa5ff46abbfd6ef50de
swati155 <72041157+swati155@users.noreply.github.com>
2021-02-21 10:50:24 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int month;
+    printf("Enter month one\n");
+    scanf("%d",&month);
+    if(month==1)
+    {
+        printf("january\n");
+        printf("number of day is 31");
+    }
+    printf("\nEnter month two\n");
+    scanf("%d",&month);
+     if(month==2)
+    {
+        printf("febraury\n");
+        printf("number of day is 28\n");
+    }
+    printf("\nEnter month three\n");
+    scanf("%d",&month);
+     if(month==3)
+    {
+        printf("march\n");
+        printf("number of day is 31");
+    }
+    printf("\nEnter month four\n");
+    scanf("%d",&month);
+     if(month==4)
+    {
+        printf("april\n");
+        printf("number of day is 30\n");
+    }
+    printf("\nEnter month five\n");
+    scanf("%d",&month);
+     if(month==5)
+    {
+        printf("may\n");
+        printf("number of day is 31\n");
+    }
+    printf("\nEnter month six\n");
+    scanf("%d",&month);
+     if(month==6)
+    {
+        printf("june\n");
+        printf("number of day is 30\n");
+    }
+    printf("\nEnter month seven\n");
+    scanf("%d",&month);
+     if(month==7)
+    {
+        printf("july\n");
+        printf("number of day is 31\n");
+    }
+    printf("\nEnter month eight\n");
+    scanf("%d",&month);
+     if(month==8)
+    {
+        printf("august\n");
+        printf("number of day is 30\n");
+    }
+    printf("\nEnter month nine\n");
+    scanf("%d",&month);
+     if(month==9)
+    {
+        printf("september\n");
+        printf("number of day is 31\n");
+    }
+    printf("\nEnter month ten\n");
+    scanf("%d",&month);
+     if(month==10)
+    {
+        printf("october\n");
+        printf("number of day is 30\n");
+    }
+    printf("\nEnter month eleven\n");
+    scanf("%d",&month);
+     if(month==11)
+    {
+        printf("november\n");
+        printf("number of day is 31\n");
+    }
+    printf("\nEnter month twelve\n");
+    scanf("%d",&month);
+     if(month==12)
+    {
+        printf("december\n");
+        printf("number of day is 30");
+    }
+    return 0;
+}
